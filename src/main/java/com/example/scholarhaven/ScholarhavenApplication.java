76c2d8127c1f587cb9726a9185d229df2c2c9c65
zunaiednudar <zunaied4821@gmail.com>
2026-02-28 20:56:22 +0600

    chore: init

---
+++ src/main/java/com/example/scholarhaven/ScholarhavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.scholarhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarhavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarhavenApplication.class, args);
+	}
+
+}
