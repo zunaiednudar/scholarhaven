4a0c4332594a759170f53dc64a62a3faef8043c1
zunaiednudar <zunaied4821@gmail.com>
2026-02-28 20:56:22 +0600

    chore: init

---
+++ src/test/java/com/example/scholarhaven/ScholarhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scholarhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
