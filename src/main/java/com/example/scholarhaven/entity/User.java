6d09b8a9836a19821d19b97245ddcd4ede40b1b3
zunaiednudar <zunaied4821@gmail.com>
2026-03-23 02:28:20 +0600

    feat(auth): AuthService.java file created + User entity updated

--- src/main/java/com/example/scholarhaven/entity/User.java
+++ src/main/java/com/example/scholarhaven/entity/User.java
@@ -14,6 +14,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
     @Column(nullable = false, unique = true)
     private String username;
 
