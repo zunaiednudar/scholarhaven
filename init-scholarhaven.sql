21640871e1daaeeb00bcc812c6f680ff4f70cfd6
zunaiednudar <zunaied4821@gmail.com>
2026-03-22 22:55:34 +0600

    feat(auth): UserDetailsServiceImpl.java file created + SQL modified (name -> username in users table)

--- init-scholarhaven.sql
+++ init-scholarhaven.sql
@@ -10,7 +10,7 @@ INSERT INTO roles (name) VALUES ('ADMIN'), ('SELLER'), ('BUYER');
 -- Create table -> 'users'
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role_id INT NOT NULL REFERENCES roles(id),
